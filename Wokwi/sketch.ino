6bce949aa0d4e1b8108988286bdb0a78f7c9820a
Mohanad <mohanad.ta.sharif@gmail.com>
2026-03-04 15:04:27 +0000

    Add initial project files for ADC and LCD integration from Wokwi simulation

---
+++ Wokwi/sketch.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);   // Change to 0x3F if your module uses that address
+
+const int adcPin = A0;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  int adcValue = analogRead(adcPin);
+  float voltage = adcValue * (5.0 / 1023.0);
+
+  // turn the ledPin on
+  digitalWrite(LED_BUILTIN, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(adcValue);
+  // turn the ledPin off:
+  digitalWrite(LED_BUILTIN, LOW);
+  // stop the program for <sensorValue> milliseconds:
+  delay(adcValue);
+
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("ADC: ");
+  lcd.print(adcValue);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Volt: ");
+  lcd.print(voltage, 3);
+  lcd.print(" V");
+
+  delay(200);
+}
